a4c75192e3afdde552f7e2d31c05876329e7ea53
Juan Pablo Vargas Rodriguez <jp-pc@Juans-MacBook-Pro-2.local>
2018-02-13 08:31:35 -0600

    Version Jp "estable"

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const fr = document.getElementById('fr');
 const ir = document.getElementById('ir');
 
 var memoria = [];
-var p = 1;  //contador del pc
+var p = 1; //contador del pc
+var finish = false;
 
 boton.addEventListener("click", click);
 
@@ -18,7 +19,10 @@ function instruction() {
     cop = (memoria[i].substr(0, 3));
     var td = (memoria[i].substr(3, 1));
     var dir = (memoria[i].substr(4, 3));
-    execute(cop, td, dir);
+    if(!finish){
+        execute(cop, td, dir);
+    }
+
   }
 
 }
@@ -27,20 +31,17 @@ function execute(cop, td, dir) {
   switch (cop) {
     case "LDA":
       switch (td) {
-        case "I":
-          ac.innerHTML = dir;
-          pc.innerHTML = ++p;
-          break;
+      break;
+    case "STA":
+      switch (td) {
         case "A":
-          ac.innerHTML = memoria[dir];
+          memoria[dir] = ac.innerHTML;
           mar.innerHTML = dir;
-          mdr.innerHTML = memoria[dir];
+          mdr.innerHTML = ac.innerHTML;
           pc.innerHTML = ++p;
           break;
         case 'D':
-          ac.innerHTML = memoria[memoria[dir]];
-          mar.innerHTML = dir;
-          mdr.innerHTML = dir;
+          memoria[memoria[dir]] = ac.innerHTML;
           mar.innerHTML = memoria[dir];
           pc.innerHTML = ++p;
           break;
@@ -49,45 +50,22 @@ function execute(cop, td, dir) {
           mar.innerHTML = dir + p;
           mdr.innerHTML = memoria[dir + p]
           break;
-        default: alert("No existe direccionamiento " + dir + " en la linea " + p);
-          break;
-      }
-      break;
-    case "STA":
-    switch (td) {
-      case "A":
-        memoria[dir] = ac.innerHTML;
-        mar.innerHTML = dir;
-        mdr.innerHTML = ac.innerHTML;
-        pc.innerHTML = ++p;
-        break;
-      case 'D':
-        memoria[memoria[dir]] = ac.innerHTML;
-        mar.innerHTML = memoria[dir];
-        pc.innerHTML = ++p;
-        break;
-      case "R":
-        ac.innerHTML = memoria[dir + p];
-        mar.innerHTML = dir + p;
-        mdr.innerHTML = memoria[dir + p]
-        break;
-      default: alert("No existe direccionamiento " + dir + " en la linea " + p);
-        break;
-    }
-
         default:
-      break;
-    }
+          alert("No existe direccionamiento " + dir + " en la linea " + p);
+      }
 
-
+    default:
   }
 
 
-function clearReg(){
+}
+
+
+function clearReg() {
   ac.innerHTML = 0;
   pc.innerHTML = 1;
- mar.innerHTML = 0;
- mdr.innerHTML = 0;
+  mar.innerHTML = 0;
+  mdr.innerHTML = 0;
   fr.innerHTML = 0;
   ir.innerHTML = 0;
 }
